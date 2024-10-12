aa45c0ead7133eb7ff50011d560722a5b8d290db
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-10-12 12:09:29 +0700

    UPDATE: Changed to browser router for vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import RootLayout from './layouts/RootLayout'
 import Home from './pages/Home'
 import NotFoundPage from './pages/404'
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<RootLayout />}>
             <Route path='/' element={<Home />} />
         </Route>
         <Route path='*' element={<NotFoundPage />}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
