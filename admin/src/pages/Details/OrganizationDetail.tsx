69e013c44cbd9fc7a58cde48e301f1cafb6e8730
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-11-01 09:08:22 +0700

    UPDATE: Added packga.json and it's dependecies

--- admin/src/pages/Details/OrganizationDetail.tsx
+++ admin/src/pages/Details/OrganizationDetail.tsx
@@ -53,7 +53,7 @@ function OrganizationDetail() {
                 <input
                   type="number"
                   placeholder="Pendidikan Terakhir"
-                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                  className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dadirk:bg-form-input dark:text-white dark:focus:border-primary"
                 />
               </div>
 
