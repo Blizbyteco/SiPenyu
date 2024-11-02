590c9d4c70e6c14f64ebc93f1436689825ce5b6f
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-11-02 12:31:38 +0700

    ADD: Added download widget for file download

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function Resources() {
   return (
-    <div className='px-4'>
-        <h1 className='text-3xl font-semibold'>Halaman Download</h1>
+    <div className='px-4 py-8 space-y-16 bg-secondary text-white'>
+      <div>
+        <h1 className='text-xl font-semibold'>Data Kepemudaan</h1>
+        <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
+      </div>
+      <div>
+        <h1 className='text-xl font-semibold'>Data Pendidikan</h1>
+        <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
+      </div>
+      <div>
+        <h1 className='text-xl font-semibold'>Data Wirausaha</h1>
+        <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
+      </div>
+      <div>
+        <h1 className='text-xl font-semibold'>Data Kecamatan</h1>
+        <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
+      </div>
+      <div>
+        <h1 className='text-xl font-semibold'>Data Organisasi</h1>
+        <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
+      </div>
     </div>
   )
 }
