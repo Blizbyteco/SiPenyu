88295ef4ed7d2a36c33364e17c3e833633d4cb92
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-11-02 20:02:47 +0700

    UPDATE: Changed font from semibold to bold

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -4,23 +4,23 @@ export default function Resources() {
   return (
     <div className='px-4 py-8 space-y-16 bg-secondary text-white'>
       <div>
-        <h1 className='text-xl font-semibold'>Data Kepemudaan</h1>
+        <h1 className='text-lg font-medium'>Data Kepemudaan</h1>
         <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
       </div>
       <div>
-        <h1 className='text-xl font-semibold'>Data Pendidikan</h1>
+        <h1 className='text-lg font-medium'>Data Pendidikan</h1>
         <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
       </div>
       <div>
-        <h1 className='text-xl font-semibold'>Data Wirausaha</h1>
+        <h1 className='text-lg font-medium'>Data Wirausaha</h1>
         <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
       </div>
       <div>
-        <h1 className='text-xl font-semibold'>Data Kecamatan</h1>
+        <h1 className='text-lg font-medium'>Data Kecamatan</h1>
         <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
       </div>
       <div>
-        <h1 className='text-xl font-semibold'>Data Organisasi</h1>
+        <h1 className='text-lg font-medium'>Data Organisasi</h1>
         <button className='px-6 py-2 bg-blue-500 text-white font-medium rounded-md text-sm mt-4'>Download</button>
       </div>
     </div>
