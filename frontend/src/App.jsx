8b61da3271746bc7cb3e1e6c0943906a59f997a4
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-11-02 14:00:21 +0700

    UPDATE: Using BrowserRouter instead of HashRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import RootLayout from './layouts/RootLayout'
 import Home from './pages/Home'
 import NotFoundPage from './pages/404'
@@ -7,15 +7,15 @@ import Resources from './pages/Resources'
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename='/SiPenyu'>
       <Routes>
         <Route path='/' element={<RootLayout />}>
             <Route path='/' element={<Home />} />
-             <Route path='downloads' element={<Resources />} />
         </Route>
+        <Route path='/downloads' element={<Resources />} />
         <Route path='*' element={<NotFoundPage />}/>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
