8a4a920dcdf4f4390b48d3703e8134b9e92b50ec
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-11-09 10:58:57 +0700

    ADD: Added cleaner for cleaning a column name

--- frontend/src/utils/data.ts
+++ frontend/src/utils/data.ts
@@ -1,4 +1,9 @@
 
+export function cleanColumnName(columnName: string) {
+    return columnName.split("_").map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()).join(" ")
+}
+
+
 
 export function extractDataColumnName(data:  Record<string, string>[]) {
     if (data.length == 0) return []
